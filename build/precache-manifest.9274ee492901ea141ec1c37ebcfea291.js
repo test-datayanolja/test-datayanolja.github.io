9810dcb3a52db69d3af2f677540e8d23e26ebf83
Dev.HyunSang <hyun.sang@parkhyunsang.com>
2020-09-19 21:29:26 +0900

    Updates

---
+++ build/precache-manifest.9274ee492901ea141ec1c37ebcfea291.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388a2f75767f1de2ed01f8905b9d5cc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25f482b9b9bbd46e0cca",
+    "url": "/static/css/main.7b21c45e.chunk.css"
+  },
+  {
+    "revision": "01be621bba26a884694a",
+    "url": "/static/js/2.0bfa8111.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.0bfa8111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f482b9b9bbd46e0cca",
+    "url": "/static/js/main.be865d36.chunk.js"
+  },
+  {
+    "revision": "e6e4165140e6058b6e4d",
+    "url": "/static/js/runtime-main.c42cc245.js"
+  },
+  {
+    "revision": "73c160630984c57e4b77b819016689ed",
+    "url": "/static/media/GothicA1-Black.73c16063.ttf"
+  },
+  {
+    "revision": "2efc91cd70fc5b47b1249c9e7ddd64d2",
+    "url": "/static/media/GothicA1-Light.2efc91cd.ttf"
+  },
+  {
+    "revision": "25ab3626b77d039abe87a29ef5e78de9",
+    "url": "/static/media/NotoSansKR-Black.25ab3626.otf"
+  },
+  {
+    "revision": "c4723de3da750cff5d95b7c9b398d983",
+    "url": "/static/media/NotoSansKR-Bold.c4723de3.otf"
+  },
+  {
+    "revision": "1b0504a3e741f8e6e07b297cb8f55d2d",
+    "url": "/static/media/NotoSansKR-Light.1b0504a3.otf"
+  },
+  {
+    "revision": "3e344d255a9c2bc603683f5b078d471d",
+    "url": "/static/media/NotoSansKR-Medium.3e344d25.otf"
+  },
+  {
+    "revision": "839a2f6c607e26dee10678a077729ba5",
+    "url": "/static/media/NotoSansKR-Regular.839a2f6c.otf"
+  },
+  {
+    "revision": "3c91862c95752f24cd41785301373e7b",
+    "url": "/static/media/NotoSansKR-Thin.3c91862c.otf"
+  },
+  {
+    "revision": "bc7c3ce46df63f10228e137625c5334f",
+    "url": "/static/media/bg.bc7c3ce4.png"
+  },
+  {
+    "revision": "752d4df06f719977d11a13a08212a802",
+    "url": "/static/media/instagram.752d4df0.png"
+  },
+  {
+    "revision": "f509d38fea30f932352d091318bdfef9",
+    "url": "/static/media/man.f509d38f.png"
+  },
+  {
+    "revision": "abd43fda84d6adb895dfcbe0d4bda905",
+    "url": "/static/media/sponsor.abd43fda.png"
+  },
+  {
+    "revision": "39843a130c07b8b250d86b7537f0bf86",
+    "url": "/static/media/title_04.39843a13.png"
+  }
+]);
