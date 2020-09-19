ffab60d1d9a354f0d1b61c8d205d5a7285d355b2
Dev.HyunSang <hyun.sang@parkhyunsang.com>
2020-09-19 20:24:50 +0900

    [ADD] 각 컴포넌트 추가

---
+++ src/components/Program/Program.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Program.css";
+
+
+class Program extends Component {
+    render() {
+        return (
+        <div className="coc-body">
+            <h1>hello</h1>
+        </div>
+        );
+    }
+}
+
+export default Program;
