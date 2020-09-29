a0e9fa89f78203a37cbd146844680313dfc3ef32
Areum Yang <s2018s10@e-mirim.hs.kr>
2020-09-29 19:46:02 +0900

    Update Menu.js

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -6,137 +6,141 @@ import $ from "jquery";
 import "../Header/Menu.css";
 
 class Menu extends Component {
-  componentDidMount() {
+componentDidMount() {
     $(".menu")
-      .each(function (index) {
+    .each(function (index) {
         console.log("each 구문 실행");
         $(this).attr("menu-index", index);
-      })
-      .click(function () {
+    })
+    .click(function () {
         var index = $(this).attr("menu-index");
         $(".menu[menu-index=" + index + "]").addClass("clicked");
-        $(".menu[menu-index=" + index + "]").before('<div id="circle"></div>');
         $(".menu[menu-index!=" + index + "]").removeClass("clicked");
-      });
-  }
+    });
+}
 
-  render() {
+render() {
     return (
-      <ul id="main-menu">
+    <ul id="main-menu">
         <li>
-          <a href="/" className="menu">
+        <a href="/" className="menu">
             ABOUT US
-          </a>
-          <ul id="sub-menu">
+        </a>
+        <ul id="sub-menu" className="about-sub">
             <li>
-              <a
-                href="/speaker"
-                onClick={function (e) {
-                  e.preventDefault();
-                  this.props.onChangeMode("speaker");
-                }.bind(this)}
-              >
-                데놀 즐기는 법
-              </a>
+                <a
+                    href="/"
+                    onClick={function (e) {
+                    e.preventDefault();
+                    this.props.onChangeMode("speaker");
+                    }.bind(this)}
+                >
+                    데놀 즐기는 방법
+                </a>
             </li>
+
             <li>
-              <a
+            <a
                 href="/"
                 onClick={function (e) {
-                  e.preventDefault();
-                  this.props.onChangeMode("partners");
+                e.preventDefault();
+                this.props.onChangeMode("partners");
                 }.bind(this)}
-              >
+            >
                 함께하는 파트너스
-              </a>
+            </a>
             </li>
-            <li>
-              <a
-                href="/"
-                onClick={function (e) {
-                  e.preventDefault();
-                  this.props.onChangeMode("members");
-                }.bind(this)}
-              >
-                2020 막강 준비위
-              </a>
-            </li>
-          </ul>
+        </ul>
         </li>
+
         <li>
-          <a href="/" className="menu">
+        <a href="/" className="menu"
+            onClick={function (e) {
+            e.preventDefault();
+            this.props.onChangeMode("concept");
+            }.bind(this)}
+        >
             EVENT
-          </a>
-          <ul id="sub-menu">
+        </a>
+        <ul id="sub-menu" className="event-sub">
             <li>
-              <a
-                href="/"
-                onClick={function (e) {
-                  e.preventDefault();
-                  this.props.onChangeMode("concept");
-                }.bind(this)}
-              >
-                데이터야 놀자 2020
-              </a>
-            </li>
-            <li>
-              <a
+            <a
                 href="/program"
                 onClick={function (e) {
-                  e.preventDefault();
-                  this.props.onChangeMode("program");
+                e.preventDefault();
+                this.props.onChangeMode("program");
                 }.bind(this)}
-              >
+            >
                 프로그램
-              </a>
+            </a>
             </li>
-          </ul>
+        </ul>
         </li>
+
         <li>
-          <a
+        <a
             href="/coc"
             className="menu"
             onClick={function (e) {
-              e.preventDefault();
-              this.props.onChangeMode("coc");
+            e.preventDefault();
+            this.props.onChangeMode("coc");
             }.bind(this)}
-          >
+        >
             COC
-          </a>
+        </a>
         </li>
+
         <li>
-          <a href="/" className="menu">
+        <a href="https://stib.ee/MWQ2" className="menu">
             NEWSLETTER
-          </a>
-          <ul id="sub-menu">
+        </a>
+        <ul id="sub-menu" className="news-sub">
             <li>
-              <a href="https://stib.ee/MWQ2">1회차</a>
+                <a
+                    href="https://stib.ee/MWQ2"
+                    onClick={function (e) {
+                    e.preventDefault();
+                    this.props.onChangeMode("program");
+                    }.bind(this)}
+                >
+                    뉴스레터 1회차
+                </a>
             </li>
+
             <li>
-              <a href="https://stib.ee/ILT2">2회차</a>
+            <a
+                href="https://stib.ee/ILT2"
+                onClick={function (e) {
+                e.preventDefault();
+                this.props.onChangeMode("program");
+                }.bind(this)}
+            >
+                뉴스레터 2회차
+            </a>
             </li>
-          </ul>
+        </ul>
         </li>
+
         <li>
-          <a href="https://event-us.kr/datayanolja2020/event" className="menu">
+        <a href="https://event-us.kr/datayanolja2020/event" className="menu">
             TICKET
-          </a>
+        </a>
         </li>
-        <li>
-          <a
+        {/* <li>
+        <a
             href="/"
             className="menu"
             onClick={function (e) {
-              e.preventDefault();
-              this.props.onChangeMode("archieve");
+            e.preventDefault();
+            this.props.onChangeMode("archieve");
             }.bind(this)}
-          >
+        >
             ARCHIEVE
-          </a>
-        </li>
-      </ul>
+        </a>
+        </li> */}
+    </ul>
     );
-  }
+}
 }
 
-export default Menu;
+export default Menu;    
