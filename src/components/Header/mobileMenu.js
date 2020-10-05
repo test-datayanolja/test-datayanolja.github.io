045754493bd9928c7f4f42fdea0801f076bc6208
areumsheep <s2018s10@e-mirim.hs.kr>
2020-10-05 10:03:46 +0900

    모바일 메뉴 완성

--- src/components/Header/mobileMenu.js
+++ src/components/Header/mobileMenu.js
@@ -6,7 +6,9 @@ import closeBtn from "../../img/Header/닫기.svg";
 
 class MobileMenu extends Component {
 componentDidMount() {
-    var click_num = 0;
+    var info_click_num = 0;
+    var denol_click_num = 0;
+    var news_click_num = 0;
     $(document).ready(function(){
         $('.menu_btn>a').on('click', function(){
             $('.menu_bg').show(); 
@@ -21,19 +23,31 @@ componentDidMount() {
                         });  
         });
 
-        $('.menu_wrap>li>a')
-        .each(function (index) {
-            $(this).attr("menu-index", index);
-        })
-
+        $('.menu-info>a')
         .on('click', function(){
-            var index = $(this).attr("menu-index");
-
-            if(click_num %2 === 0)
-                $('.mobile-menu-clicked>ul').css('display','block');
+            if(info_click_num %2 === 0)
+                $('.mobile-menu-info-clicked>ul').css('display','block');
             else
-                $('.mobile-menu-clicked>ul').css('display','none');
-            click_num ++;
+                $('.mobile-menu-info-clicked>ul').css('display','none');
+            info_click_num ++;
+        });
+
+        $('.menu-2020>a')
+        .on('click', function(){
+            if(denol_click_num %2 === 0)
+                $('.mobile-menu-2020-clicked>ul').css('display','block');
+            else
+                $('.mobile-menu-2020-clicked>ul').css('display','none');
+            denol_click_num ++;
+        });
+
+        $('.menu-news>a')
+        .on('click', function(){
+            if(news_click_num %2 === 0)
+                $('.mobile-menu-news-clicked>ul').css('display','block');
+            else
+                $('.mobile-menu-news-clicked>ul').css('display','none');
+            news_click_num ++;
         });
     });
 }
@@ -63,13 +77,14 @@ render() {
             </div>
 
             <ul className="menu_wrap">
-                <li>
+
+                <li className="menu-info">
                     <a href="/" onClick={function (e) {
                         e.preventDefault();
                         this.props.onChangeMode("main");
                     }.bind(this)}>데놀랜드 소개</a>
 
-                    <div className="mobile-menu-clicked">
+                    <div className="mobile-menu-info-clicked">
                         <ul>
                             <li><a
                                 href="/"
@@ -91,13 +106,13 @@ render() {
                     </div>
                 </li>
 
-                <li>
+                <li className="menu-2020">
                     <a href="/" onClick={function (e) {
                         e.preventDefault();
                         this.props.onChangeMode("concept");
                     }.bind(this)}>데놀랜드 2020</a>
 
-                    <div className="mobile-menu-clicked">
+                    <div className="mobile-menu-2020-clicked">
                         <ul>
                             <li><a href="/" onClick={function (e) {
                                 e.preventDefault();
@@ -115,13 +130,13 @@ render() {
                     }.bind(this)}>데놀랜드 이용규칙</a>
                 </li>
 
-                <li>
+                <li className="menu-news">
                     <a href="/" onClick={function (e) {
                         e.preventDefault();
                         this.props.onChangeMode("main");
                     }.bind(this)}>데놀랜드 뉴스레터</a>
 
-                    <div className="mobile-menu-clicked">
+                    <div className="mobile-menu-news-clicked">
                         <ul>
                             <li><a href="https://stib.ee/MWQ2">뉴스레터 1회차</a></li>
                             <li><a href="https://stib.ee/ILT2">뉴스레터 2회차</a></li>
